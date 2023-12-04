a7831a6ab3fe3ba9a4f869fa612b860fe6dd367b
ryugeumjung <fbgjung@gmail.com>
2023-12-04 23:01:13 +0900

    문제1: 절차 지향 직사각형 프로그램을 객체 지향으로 변경하기 - 정답

--- src/oop1/ex/Rectangle.java
+++ src/oop1/ex/Rectangle.java
@@ -3,16 +3,14 @@ package oop1.ex;
 public class Rectangle {
     int width;
     int height;
-    boolean isSquare;
 
-    void calculateArea() {
-        System.out.println("넓이: " + width * height);
+    int calculateArea() {
+        return width * height;
     }
-    void calculatePerimeter() {
-        System.out.println("둘레 길이: " + 2 * (width + height));
+    int calculatePerimeter() {
+        return 2 * (width + height);
     }
-    void isSquare() {
-        isSquare = width == height;
-        System.out.println("정사각형 여부: " + isSquare);
+    boolean isSquare() {
+        return this.width == this.height;
     }
 }
