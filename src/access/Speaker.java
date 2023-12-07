fa38bb71607e40f5172d9117ea4fea3636d9688e
ryugeumjung <fbgjung@gmail.com>
2023-12-07 23:22:44 +0900

    접근 제어자 이해1 volume 필드를 Speaker 클래스 외부에서 접근하는 문제 발생

---
+++ src/access/Speaker.java
@@ -0,0 +1,27 @@
+package access;
+
+public class Speaker {
+    int volume;
+
+    // 생성자를 통해 초기 음략 값 지정
+    Speaker(int volume){
+        this.volume = volume;
+    }
+    void volumeUp(){
+        if(volume >= 100){
+            System.out.println("음량을 증가할 수 없습니다. 최대 음량입니다.");
+        } else{
+            volume += 10;
+            System.out.println("음량을 10 증가합니다.");
+        }
+    }
+
+    void volumeDown(){
+        volume -= 10;
+        System.out.println("volumeDown 호출");
+    }
+
+    void showVolume() {
+        System.out.println("현재 음량:" + volume);
+    }
+}
